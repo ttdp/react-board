dda4db7600e6fa8168ce0e34f6c4b66d40278ccc
ttdp <tiantong2@126.com>
2022-12-24 09:32:17 +0800

    add bg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useMemo, useState } from "react"
+import { getRandomImage } from "./utils/image"
+import clsx from "clsx"
+
 function App() {
+
+  const randImg = useMemo(
+    () => getRandomImage({ seeds: ["wallpaper", "nature", "abstract"] }),
+    []
+  )
+
   return (
-    <div>
-        <h2>Here we go!</h2>
+    <div
+      className={clsx(
+        "App flex flex-col min-h-screen bg-cover bg-center",
+        false && "dark"
+      )}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${randImg})`,
+      }}>
+      <h2>Here we go</h2>
     </div>
   )
 }
